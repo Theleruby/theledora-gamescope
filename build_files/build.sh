633e6a1ac9b72829971542bbd35bc5c72f1b363d
Ruby Dennington <theleruby@gmail.com>
2026-04-26 18:03:58 +0100

    Add packages to build.sh based on NVIDIA desktop build Most development packages were removed as they shouldn't need to be used

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,23 +2,87 @@
 
 set -ouex pipefail
 
-### Install packages
+# make sure /var/opt directory exists so we can install stuff into it
+mkdir -p /var/opt
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
+# make sure all the fedora repo definitions are installed as bazzite doesn't include these
+dnf5 install -y fedora-workstation-repositories
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+# make sure rpmfusion is enabled
+dnf5 config-manager setopt rpmfusion-free.enabled=1
+dnf5 config-manager setopt rpmfusion-free-updates.enabled=1
+dnf5 config-manager setopt rpmfusion-nonfree.enabled=1
+dnf5 config-manager setopt rpmfusion-nonfree-updates.enabled=1
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+# htop
+dnf5 install -y htop
 
-#### Example for enabling a System Unit File
+# dependencies for some stuff
+dnf5 install -y kdsingleapplication-qt6
 
-systemctl enable podman.socket
+# easyeffects
+dnf5 install -y easyeffects calf lv2 lv2-calf-plugins lv2-mdala-plugins lv2-zam-plugins lsp-plugins-lv2
+
+# alsa plugin for dolby digital
+dnf5 install -y alsa-plugins-a52
+
+# python
+dnf5 install -y python3-devel
+
+# chrome
+dnf5 config-manager setopt google-chrome.enabled=1
+dnf5 install -y google-chrome-stable
+
+# mercurial
+dnf5 install -y mercurial tortoisehg python3-dulwich kdiff3
+
+# fluidsynth
+dnf5 install -y fluidsynth fluid-soundfont-common fluid-soundfont-gm
+
+# vlc
+dnf5 install -y vlc vlc-plugins-all vlc-plugin-kde vlc-plugin-notify vlc-plugin-pipewire vlc-plugins-freeworld
+
+# yt-dlp
+dnf5 install -y yt-dlp
+
+# zerotier
+dnf5 install -y zerotier-one
+
+# libreoffice
+dnf5 install -y libreoffice libreoffice-kf6 libreoffice-help-en
+
+# discord
+dnf5 install -y discord
+
+# audacity
+dnf5 install -y audacity-freeworld
+
+# avidemux
+dnf5 install -y avidemux
+
+# krusader
+dnf5 install -y krusader
+
+# some misc kde productivity apps that are useful
+dnf5 install -y kolourpaint krita kdenlive kamoso
+
+# disk utils similar to crystaldisk
+dnf5 install -y kdiskmark
+dnf5 copr -y enable birkch/QDiskInfo
+dnf5 install -y QDiskInfo
+
+# fonts
+dnf5 install -y cabextract fontconfig
+dnf5 install -y gnu-free-fonts-common gnu-free-sans-fonts lpf-cleartype-fonts lpf-mscore-fonts lpf-mscore-tahoma-fonts
+rm /usr/share/applications/lpf*.desktop
+
+# move stuff in /var/opt to /usr/lib/opt and add symlink to tmpfiles conf
+# taken from https://github.com/astrovm/amyos/blob/main/build_files/fix-opt.sh, thanks <3
+for dir in /var/opt/*/; do
+  [ -d "$dir" ] || continue
+  dirname=$(basename "$dir")
+  mv "$dir" "/usr/lib/opt/$dirname"
+  echo "L+ /var/opt/$dirname - - - - /usr/lib/opt/$dirname" >>/usr/lib/tmpfiles.d/move-opt-files.conf
+done
+
+dnf5 clean all
